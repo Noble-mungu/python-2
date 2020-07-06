7a799719f00b89499faf13231ae6191f5f3c2794
Noble Mungu <mungunoble@gmail.com>
2020-07-06 13:00:43 +0300

    create a test case

---
+++ app/models.py
@@ -0,0 +1,21 @@
+class Source:
+	'''
+	Source class to define source objects
+	'''
+	def __init__(self,id,name,description,url,category):
+		'''
+		Function to initialize source Objects
+		Args:
+		1.id
+		2.name
+		3.description
+		4.url
+		5.category
+		'''
+		self.id = id 
+		self.name = name
+		self.description = description
+		self.url = url
+		self.category = category
+		
+
